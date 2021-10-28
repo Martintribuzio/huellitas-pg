cf520c21eddf633c9165d5137b5dcbc5b8cc287e
benjaaracil <benjaminaracil@gmail.com>
2021-10-28 12:40:51 -0300

    App.js modified

--- api/src/app.js
+++ api/src/app.js
@@ -12,6 +12,23 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
 
+//Extra configs
+app.use(session({
+  secret: "mysecretsession",
+  resave: false,
+  saveUninitialized: false
+})); //Investigar en Documentacion de session!!
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next)=>{
+  app.locals.signupMessage = req.flash("signupMessage");
+  app.locals.signinMessage = req.flash("signinMessage");
+  app.locals.user = req.user;
+  next();
+})
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true')
