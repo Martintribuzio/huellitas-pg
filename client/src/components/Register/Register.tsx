c1f8c332c86f7902e063946a965c8de83277bd59
Cervera María Guadalupe <guadacervera8@gmail.com>
2021-10-29 09:18:32 -0300

    Campos agregados al registro

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import {yupResolver}from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { TextField } from "@material-ui/core";
 import axios from "axios";
 import Swal from "sweetalert2";
 import "./Register.css";
 
 type Data = {
-  /*  name:string;
-  lastname:string; */
+   name:string;
+  lastname:string;
   email: string;
   /* username: string; */
   password: string;
   confirmPassword: string;
 };
+const schema=yup.object().shape({
+  name:yup.string().required('Ingresa tu nombre'),
+  lastname:yup.string().required('Ingresa tu apellido'),
+  email:yup.string().email().required(),
+  password:yup.string().min(8).max(20).required(),
+  confirmPassword:yup.string().oneOf([yup.ref('password'),null],'Las contraseñas no coinciden').required()
+});
 
 function Register() {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
-  } = useForm<Data>();
+  } = useForm<Data>(({resolver:yupResolver(schema)}));
 
   const onSubmit = handleSubmit((data) => {
     /* alert(JSON.stringify(data))  */
@@ -38,65 +49,107 @@ function Register() {
       })
       .catch((error) => console.log(error.message));
   });
-  const validate=(value:string|null)=>{
-    /* if(value===document.getElementById('password':String).value) */
-  
-  }
-  
+
 
   return (
     <div className="container">
       <h1>Registrate</h1>
       <form onSubmit={onSubmit}>
-        {/* <div>
-        <input {...register('name',{ required: true })} id="name" name="name" type="text" placeholder='Nombre'/>
-        
-      </div>
-      <div>
-        <input {...register('lastname',{ required: true })} id="lastname" name="lastname" type="text" placeholder='Apellido'/>
-        
-      </div> */}
         <div>
-          <input
-            {...register("email", {
-              required: true,
-              pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-            })}
-            id="email"
-            name="email"
-            type="text"
-            placeholder="E-mail"
+        <Controller
+        name="name"
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label="Nombre"
+            variant="outlined"
+            error={!!errors.name}
+            helperText={errors.name ? errors.name.message : ''}
+            fullWidth
+            margin="dense"
           />
-          {errors.email && <p>Email invalido</p>}
+        )}
+      />
+        </div>
+        <div>
+        <Controller
+        name="lastname"
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label="Apellido"
+            variant="outlined"
+            error={!!errors.lastname}
+            helperText={errors.lastname ? errors.lastname.message : ''}
+            fullWidth
+            margin="dense"
+          />
+        )}
+      />
+        </div>
+      
+        <div>
+        <Controller
+            name="email"
+            control={control}
+            defaultValue="example@dev.com"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Email"
+                variant="outlined"
+                error={!!errors.email}
+                helperText={errors.email ? errors.email.message : ''}
+                fullWidth
+                margin="dense"
+              />
+            )}
+          />
         </div>
         {/*  <div>
         <input {...register('username',{ required: true })} id="username" name="username" type="text" placeholder='Usuario'/>
       </div> */}
         <div>
-          <input
-            {...register("password", { required: true, minLength: 8 })}
-            id="password"
+        <Controller
             name="password"
-            type="password"
-            placeholder="Contraseña"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                {...field}
+                type="password"
+                label="Password"
+                variant="outlined"
+                error={!!errors.password}
+                helperText={errors.password ? errors.password?.message : ''}
+                fullWidth
+                margin="dense"
+              />
+            )}
           />
-          {errors.password && (
-            <p>La contraseña debe tener al menos 8 caracteres</p>
-          )}
         </div>
         <div>
-          <input
-            {...register("confirmPassword", {
-              required: true,
-              minLength: 8,
-              /* validate:{validate(value)} */
-            })}
-            id="confirmPassword"
+        <Controller
             name="confirmPassword"
-            type="password"
-            placeholder="Confirmar contraseña"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                {...field}
+                type="password"
+                label="Confirma tu contraseña"
+                variant="outlined"
+                error={!!errors.confirmPassword}
+                helperText={errors.confirmPassword ? errors.confirmPassword.message : ''}
+                fullWidth
+                margin="dense"
+              />
+            )}
           />
-          {errors.confirmPassword && <p>Repite la contraseña ingresada</p>}
         </div>
         <p>¿Ya tienes cuenta?</p>
         <Link to="/">Ingresa</Link>
