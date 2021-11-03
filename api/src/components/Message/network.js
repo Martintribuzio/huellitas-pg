2bba04d1dbd466fdf0368e6e8193038d80463476
JustBecerra <justj.becerra@gmail.com>
2021-11-03 14:56:30 -0300

    agregando rutas

--- api/src/components/Message/network.js
+++ api/src/components/Message/network.js
@@ -1,13 +1,25 @@
 const messageNetwork = require('express').Router()
+const { message } = require('../../models/Message')
 const createMessage = require('./controller')
 
 messageNetwork.post('/', async (req, res) => {
     try{
-      const message = await createMessage(req.body)
-      res.send(message)
+      const Message = await createMessage(req.body)
+      res.send(Message)
     }catch(err){
       res.send(err.message)
     }
 })
 
-module.exports = messageNetwork
+messageNetwork.get('/:Converseid', async (req, res) => {
+    try{
+        const messages = await message.find({
+            Converseid: req.params.Converseid
+        });
+        res.status(200).send(messages)
+    } catch(err){
+        res.status(404).json(err)
+    }
+})
+
+module.exports = message
