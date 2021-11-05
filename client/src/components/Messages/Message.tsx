c831ff03af7b8108f895e108ce970e7e68bffa2c
JustBecerra <justj.becerra@gmail.com>
2021-11-04 23:09:42 -0300

    cambie el message

--- client/src/components/Messages/Message.tsx
+++ client/src/components/Messages/Message.tsx
@@ -1,15 +1,21 @@
-export default function Message() {
+interface prop{
+  text:string;
+  own:boolean
+}
+
+export default function Message(prop:prop): JSX.Element {
     return (
-      <div /* className={own ? "message own" : "message"} */>
+      <>
+      <div className={prop.own ? "message own" : "message"} >
         <div className="messageTop">
           <img
             className="messageImg"
             src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
-          {/* <p className="messageText">{message.text}</p> */}
+          <p className="messageText">{prop.text}</p>
         </div>
-        {/* <div className="messageBottom">{format(message.createdAt)}</div> */}
       </div>
+      </>
     );
   }
