7ab7b7f44e3f7cda55df35d90b91d553a0ee2c23
JustBecerra <justj.becerra@gmail.com>
2021-11-05 13:09:01 -0300

    anda el chaaaat

--- client/src/components/Messages/Message.tsx
+++ client/src/components/Messages/Message.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import axios from "axios"
 import style from './Message.module.css'
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input'
 
 interface message{
   content: string;
@@ -9,18 +11,17 @@ interface message{
   sender: string;
 }
 
-
-
 export default function Message(){
-  const [message,setMessage] = useState<message[]>()
+  const [messages,setMessages] = useState<message[]>()
+  const [newMessage,setnewMessage] = useState<string>()
+  
+  const idSender = localStorage.getItem('userId');
   const { ConverseId } = useParams<{ConverseId?:string}>()
   useEffect(() => {
     const getMessage= async () => {
       try{
-          console.log('ID CONVERSACION',ConverseId)
           const res = await axios.get(`/message/${ConverseId}`)
-          console.log(res.data)
-          setMessage(res.data)
+          setMessages(res.data)
       }catch(err:any){
           return err.message
       }
@@ -28,7 +29,24 @@ export default function Message(){
     getMessage()
   }, [ConverseId])
   
-     
+  const handleSubmit = async (e:any) => {
+    try{
+      e.preventDefault()
+      const message = {
+        sender:idSender,
+        content: newMessage,
+        conversationId: ConverseId
+      }
+      const res = await axios.post("/message", message)
+      console.log(res.data)
+      if(messages !== undefined){
+        setMessages([...messages, res.data])
+      }
+    }catch(err:any){
+      console.log(err.message)
+    }
+  }   
+
   return (
       <>
       <div /* className={params.own ? "message own" : "message"} */ >
@@ -38,10 +56,22 @@ export default function Message(){
             src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           /> */}
-          {message?.map((c) => (
+          {messages?.map((c) => (
              <p>{c.content}</p>
           ))}
         </div>
+        <div className={style.inputSubmit}>
+          <div className={style.inputChat}>
+            <Input 
+              placeholder="Escribe un mensaje" 
+              onChange={(e) => setnewMessage(e.target.value)}
+              value={newMessage}  
+            />
+          </div>
+            <div className={style.boton}>
+              <Button onClick={handleSubmit}>enviar</Button>
+            </div>
+          </div>
       </div>
       </>
     );
--- client/src/components/Messenger/Messenger.tsx
+++ client/src/components/Messenger/Messenger.tsx
@@ -11,42 +11,25 @@ import ListSubheader from '@mui/material/ListSubheader';
 import Avatar from '@mui/material/Avatar';
 import Message from '../Messages/Message';
 import Input from '@mui/material/Input'
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import style from './Messenger.module.css'
-import axios from 'axios'
 import Conversations from '../conversations/Conversations';
-import getPostsUser from '../../services/getPostsUser';
 import { useSelector } from 'react-redux';
 import { typeState } from '../../redux/reducers/index';
 import { conversation } from '../../redux/types/types';
-import Box from '@mui/material/Box';
 
-interface menssage{
-  content: string,
-  sender: string
-}
 
 export default function BottomAppBar() {
     const [search, setSearch] = useState<string>('');
-    // const {currentChat, setcurrentChat} = useState<conversation>()
     const id = localStorage.getItem('userId');
 
     const convers:Array<conversation> = useSelector((state:typeState) => state.conversations);
-  
-    interface converseichon{
-      members:Array<string>
-    }
-
-    // interface message{
-    //   content:string
-    //   sender:string
-    // }
-    
   
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
       e.preventDefault();
       setSearch(e.target.value);
     }
+
     if(!Array.isArray(convers)){
       return(
         <div>
@@ -77,14 +60,6 @@ export default function BottomAppBar() {
                   ))} */}
                   <Message />
                </div>
-             <div className={style.inputSubmit}>
-               <div className={style.inputChat}>
-                  <Input placeholder="Escribe un mensaje" />
-               </div>
-               <div className={style.boton}>
-                  <Button>enviar</Button>
-               </div>
-             </div>
              </div>
             {/* <List sx={{ mb: 2 }} > /}
               {/ {messages.slice(0,5).map(({ id, primary, secondary, person }) => (
