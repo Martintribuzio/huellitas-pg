653cde89207ad40cb25e1d8727f9b56d8ddeeb20
Martin <martinmozzt@pm.me>
2021-11-07 18:23:57 -0300

    CHATTTTTTTTTTTTTTTTTT PARA PROBAR DIPLOIO

--- Socket/index.js
+++ Socket/index.js
@@ -1,51 +1,72 @@
-const io = require('socket.io')(
-    3002,{
-        cors:{
-            origin:'http://localhost:3000'
-        }
-    }
-);
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3002;
+const server = http.createServer(app);
+
+const io = socketio(server, {
+  cors: {
+    origins: '*',
+  },
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// const io = require('socket.io')(3002, {
+//   cors: {
+//     origin: 'http://localhost:3000/',
+//   },
+// });
+
 let users = [];
 
-const addUser = (id,socketId) => {
-    !users.some(user => user.id === id)&&
-        users.push({id,socketId});
-}
-const removeUser = (id) => {
-    users = users.filter(user => user.id !== id);
-}
-const getUser = (receiverId) => {
-    console.log("log de receiverId", receiverId)
-    return users.find((user) => user.id === receiverId);
-  };
+const addUser = (id, socketId) => {
+  !users.some(user => user.id === id) && users.push({ id, socketId });
+};
 
-io.on('connection',(socket)=>{
-    console.log("a new user has connected");
-    
-    // io.emit("welcome", "hello there")
+const removeUser = id => {
+  users = users.filter(user => user.id !== id);
+};
 
-    socket.on('addUser',(id)=>{
-        addUser(id,socket.id)
-        console.log("que carajo es este id " ,id)
-        console.log("usuarios = ", users)
-        io.emit('getUsers',users)
-    })
+const getUser = receiverId => {
+  console.log('log de receiverId', receiverId);
+  return users.find(user => user.id === receiverId);
+};
 
-    socket.on("sendMessage", ({ senderId,receiverId, text }) => {
-        const user = getUser(receiverId);
-        console.log(user)
-        if(user.socketId){
-            io.emit("getMessage", {
-            senderId,
-            text,
-            });
-        }
+io.on('connection', socket => {
+  console.log('a new user has connected');
+
+  // io.emit("welcome", "hello there")
+
+  socket.on('addUser', id => {
+    addUser(id, socket.id);
+    console.log('que carajo es este id ', id);
+    console.log('usuarios = ', users);
+    io.emit('getUsers', users);
+  });
+
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    console.log('ID', receiverId);
+    const user = getUser(receiverId);
+    console.log(user);
+    if (user.socketId) {
+      io.emit('getMessage', {
+        senderId,
+        text,
       });
-    
-    
-    socket.on('disconnect',()=>{
-        console.log('user disconnected');
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-    });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    removeUser(socket.id);
+    io.emit('getUsers', users);
+  });
 });
--- client/src/components/Messages/Message.tsx
+++ client/src/components/Messages/Message.tsx
@@ -1,118 +1,126 @@
-import { useEffect, useRef, useState } from "react"
-import { useParams } from "react-router"
-import { useSelector } from "react-redux"
-import axios from "axios"
-import style from './Message.module.css'
+import { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import style from './Message.module.css';
 import Button from '@mui/material/Button';
-import Input from '@mui/material/Input'
-import { CollectionsOutlined } from "@mui/icons-material";
-import {io} from "socket.io-client";
+import Input from '@mui/material/Input';
+import { CollectionsOutlined } from '@mui/icons-material';
+import { io } from 'socket.io-client';
 import SendIcon from '@mui/icons-material/Send';
 import { typeState } from '../../redux/reducers/index';
 
-interface message{
+interface message {
   content: string;
   Converseid: string;
   sender: string;
 }
 
-export default function Message(){
-  const [messages,setMessages] = useState<message[]>()
-  const [newMessage,setnewMessage] = useState<string>()
-  const [arrivalMessage,setArrivalMessage] = useState<any>();
-  const socket:any = useRef()
+export default function Message() {
+  const [messages, setMessages] = useState<message[]>();
+  const [newMessage, setnewMessage] = useState<string>();
+  const [arrivalMessage, setArrivalMessage] = useState<any>();
+  const socket: any = useRef();
   const idSender = localStorage.getItem('userId');
-  const { ConverseId } = useParams<{ConverseId:string}>()
+  const { ConverseId } = useParams<{ ConverseId: string }>();
 
-  const convers:any = useSelector((state:typeState) => state.conversations).filter(elem => elem._id === ConverseId)[0];
-  
+  const convers: any = useSelector(
+    (state: typeState) => state.conversations
+  ).filter(elem => elem._id === ConverseId)[0];
+
   useEffect(() => {
-    const getMessage= async () => {
-      try{
-          const res = await axios.get(`/message/${ConverseId}`)
-          setMessages(res.data)
-      }catch(err:any){
-          return err.message
+    const getMessage = async () => {
+      try {
+        const res = await axios.get(`/message/${ConverseId}`);
+        setMessages(res.data);
+      } catch (err: any) {
+        return err.message;
       }
-    }
-    getMessage()
-  }, [ConverseId])
-  
+    };
+    getMessage();
+  }, [ConverseId]);
+
   useEffect(() => {
-    socket.current = io("ws://localhost:3002")
-    socket.current.on("getMessage", (data:any) => {
-      console.log('DATA QUE MANDA EL SOCKET' ,data)
+    socket.current = io(
+      process.env.REACT_APP_SOCKET_URL || 'http://localhost:3002'
+    );
+    socket.current.on('getMessage', (data: any) => {
+      console.log('DATA QUE MANDA EL SOCKET', data);
       setArrivalMessage({
         sender: data.senderId,
-        content: data.text
-      })
-    })
-  },[])
-  
+        content: data.text,
+      });
+    });
+  }, []);
+
   useEffect(() => {
-    if(messages !== undefined){
-      arrivalMessage && convers?.members.includes(arrivalMessage.sender) && setMessages((prev:any) => [...prev, arrivalMessage]);
+    if (messages !== undefined) {
+      arrivalMessage &&
+        convers?.members.includes(arrivalMessage.sender) &&
+        setMessages((prev: any) => [...prev, arrivalMessage]);
     }
-  },[arrivalMessage,convers])
+  }, [arrivalMessage, convers]);
 
   useEffect(() => {
-    socket.current.emit("addUser",idSender)
-  },[localStorage])
+    socket.current.emit('addUser', idSender);
+  }, [localStorage]);
 
-  const receiverId = convers?.members?.find((member:string) => member !== idSender)
-  console.log("este es receiverId ", receiverId)
+  const receiverId = convers?.members?.find(
+    (member: string) => member !== idSender
+  );
+  console.log('este es receiverId ', receiverId);
 
-  const handleSubmit = async (e:any) => {
-    try{
-      e.preventDefault()
-      socket.current.emit("sendMessage", {
+  const handleSubmit = async (e: any) => {
+    try {
+      e.preventDefault();
+      socket.current.emit('sendMessage', {
         senderId: idSender,
-        receiverId:receiverId,
-        text:newMessage
-      })
+        receiverId: receiverId,
+        text: newMessage,
+      });
       const message = {
-        sender:idSender,
+        sender: idSender,
         content: newMessage,
-        Converseid: ConverseId
+        Converseid: ConverseId,
+      };
+      const res = await axios.post('/message', message);
+      if (messages !== undefined) {
+        setMessages([...messages, res.data]);
+        setnewMessage('');
       }
-      const res = await axios.post("/message", message)
-      if(messages !== undefined){
-        setMessages([...messages, res.data])
-        setnewMessage('')
-      }
-    }catch(err:any){
-      console.log(err.message)
+    } catch (err: any) {
+      console.log(err.message);
     }
-  }   
-  
+  };
 
   return (
-      <div >
-        <div /* className={style.mensaje} */ > 
-         <div>
-           <img
-            className="messageImg"
+    <div>
+      <div /* className={style.mensaje} */>
+        <div>
+          <img
+            className='messageImg'
             // src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-          /> 
-           {messages?.map((c) => (
-             <div className={c.sender!==idSender?'other':'own'}>
-               <p>{c.content}</p></div>
+            alt=''
+          />
+          {messages?.map(c => (
+            <div className={c.sender !== idSender ? 'other' : 'own'}>
+              <p>{c.content}</p>
+            </div>
           ))}
         </div>
         <div className={style.inputSubmit}>
           <div className={style.inputChat}>
-            <Input 
-              placeholder="Escribe un mensaje" 
-              onChange={(e) => setnewMessage(e.target.value)}
-              value={newMessage}  
+            <Input
+              placeholder='Escribe un mensaje'
+              onChange={e => setnewMessage(e.target.value)}
+              value={newMessage}
             />
           </div>
-            <div className={style.boton}>
-              <SendIcon onClick={handleSubmit}></SendIcon>
-            </div>
-           </div>
-          </div> 
+          <div className={style.boton}>
+            <SendIcon onClick={handleSubmit}></SendIcon>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
