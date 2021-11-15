125e8889166446b54ceabd0304670f02b28cf330
cristian <cdwenz@gmail.com>
2021-11-15 16:24:59 -0300

    perfilShelter

--- api/src/components/user/store.js
+++ api/src/components/user/store.js
@@ -65,7 +65,12 @@ const getSheltersDB = async () => {
 
 const getShelterDetDB = async (id) => {
   try {
-    const shelter = await User.findById(id).populate('profileImage');
+    const shelter = await User.findById(id).populate('profileImage').populate({
+      path: 'posts',
+      populate: {
+        path: 'petImage',
+      },
+    });
     return shelter;
   } catch (error) {
     throw new Error(error);
